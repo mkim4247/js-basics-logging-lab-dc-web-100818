53db67f9b7d89047692f0b764f510f80b97b1b31
Michael Kim <m.kim4247@gmail.com>
2020-05-13 15:00:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const driver = 'bob';
 
-console.log('this code was called')
+console.log('this code was called');
 
-console.log(`${driver}`)
+console.log(driver);
 
-console.log(`${driver}`, `is the driver variable name`)
+console.log(driver, 'is the driver variable name');
